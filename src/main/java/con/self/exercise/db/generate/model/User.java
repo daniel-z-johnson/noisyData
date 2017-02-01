1fee453cee84d600f028aa1c38245c6894ebd94e
prime23 <danzaljohnson@gmail.com>
2017-01-31 22:19:02 -0700

    Added simple user model

---
+++ src/main/java/con/self/exercise/db/generate/model/User.java
@@ -0,0 +1,93 @@
+package con.self.exercise.db.generate.model;
+
+
+import javax.persistence.*;
+import java.util.UUID;
+
+/**
+ * Created by prime23 on 1/31/17.
+ */
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "key")
+    private UUID apiKey;
+
+    public User() {
+        // empty constructor for hibernate
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public UUID getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(UUID apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != user.id) return false;
+        if (firstName != null ? !firstName.equals(user.firstName) : user.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(user.lastName) : user.lastName != null) return false;
+        return apiKey != null ? apiKey.equals(user.apiKey) : user.apiKey == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (apiKey != null ? apiKey.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", apiKey=" + apiKey +
+                '}';
+    }
+}
