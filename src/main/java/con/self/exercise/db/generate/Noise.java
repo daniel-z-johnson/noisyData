d7f0288daaa1c58d505950f8bd7b8f3c9658b662
prime23 <danzaljohnson@gmail.com>
2017-01-31 22:15:59 -0700

    Initial commit

---
+++ src/main/java/con/self/exercise/db/generate/Noise.java
@@ -0,0 +1,14 @@
+package con.self.exercise.db.generate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by prime23 on 1/31/17.
+ */
+@SpringBootApplication
+public class Noise {
+    public static void main(String[] args) {
+        SpringApplication.run(Noise.class, args);
+    }
+}
