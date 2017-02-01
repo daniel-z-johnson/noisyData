11e3121f365b073870347cdecf2f035457031138
prime23 <danzaljohnson@gmail.com>
2017-01-31 22:22:51 -0700

    Added util class to get common first and last names

---
+++ src/main/java/con/self/exercise/db/generate/util/NamesUtil.java
@@ -0,0 +1,46 @@
+package con.self.exercise.db.generate.util;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by prime23 on 1/31/17.
+ */
+public final class NamesUtil {
+    private NamesUtil(){
+        // util class should be instantiated
+    }
+
+    public static List<String> firstNames() throws IOException {
+        List<String> names = new ArrayList<String>();
+        Document doc = Jsoup.connect("https://www.babble.com/pregnancy/1000-most-popular-boy-names/").get();
+        Elements list = doc.select("main li");
+        for(Element e : list) {
+            names.add(e.text());
+        }
+        doc = Jsoup.connect("https://www.babble.com/pregnancy/1000-most-popular-girl-names/").get();
+        list = doc.select("main li");
+        for(Element e : list) {
+            names.add(e.text());
+        }
+        return names;
+    }
+
+    public static List<String> lastNames() throws IOException {
+        List<String> names = new ArrayList<String>();
+        Document doc = Jsoup.connect("http://names.mongabay.com/data/1000.html").get();
+        Elements list = doc.select("tr td:nth-child(1)");
+        for(Element e : list) {
+            String name = e.text();
+            name = name.substring(0,1) + name.substring(1).toLowerCase();
+            names.add(name);
+        }
+        return names;
+    }
+}
